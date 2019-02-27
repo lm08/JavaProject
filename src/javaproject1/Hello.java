18b4b51d303904013ad1cd50bf77a41c6c104914
moseshoward18 <35472698+moseshoward18@users.noreply.github.com>
2019-02-27 12:21:57 -0800

    Update Hello.java
    moses branch

--- src/javaproject1/Hello.java
+++ src/javaproject1/Hello.java
@@ -15,6 +15,7 @@ public class Hello {
         System.out.println("Matt was here");
         System.out.println("Tony is here finally HELLO EVERYONE!");
         System.out.println("Moses cannot be silenced!");
+        System.out.println("Moses' branch is the best!")
     }
     
 }
