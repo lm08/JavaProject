6c7d001a510eaab50ee24abcba3cb5a833e628cc
Matt H <matthamel321@gmail.com>
2019-02-27 12:14:58 -0800

    Added conflict.png

--- src/javaproject1/Hello.java
+++ src/javaproject1/Hello.java
@@ -10,7 +10,7 @@ package javaproject1;
  * @author Student
  */
 public class Hello {
-    public static void main(String args[]){
+    public static void main(String[] args){
         System.out.println("hello");
         System.out.println("Matt was here");
         System.out.println("Moses was here");
